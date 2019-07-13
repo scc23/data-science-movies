44ef0122eede68cf4239b9c1bff254ac4134f51a
scc23 <sherman_chow@sfu.ca>
2019-07-13 15:47:38 -0700

    Compute correlation coefficients

--- q1/movies_correlation.py
+++ q1/movies_correlation.py
@@ -5,6 +5,13 @@
 import sys
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import re
+from scipy import stats
+
+# pd.set_option('display.max_rows', 500)
+# pd.set_option('display.max_columns', 500)
+# pd.set_option('display.width', 1000)
 
 
 def main():
@@ -25,7 +32,6 @@ def main():
     ]
     omdb = omdb.sort_values(by=['imdb_id'])
     omdb = omdb.set_index('imdb_id')
-    # print(omdb)
     
     # print(rotten.columns.values)
     rotten = rotten[
@@ -39,7 +45,6 @@ def main():
     ]
     rotten = rotten.sort_values(by=['imdb_id'])
     rotten = rotten.set_index('imdb_id')
-    # print(rotten)
 
     # print(wikidata.columns.values)
     wikidata = wikidata[
@@ -49,12 +54,37 @@ def main():
     ]
     wikidata = wikidata.sort_values(by=['imdb_id'])
     wikidata = wikidata.set_index('imdb_id')
-    # print(wikidata)
 
 
     # Join the DataFrames by index (imdb_id)
     movies = omdb.join(rotten).join(wikidata)
-    print(movies)
+
+    movies['critic_average'] = movies['critic_average'].apply(lambda x: x / 2)
+    # movies['audience_reviews'] = movies[['audience_average', 'audience_percent']].apply(tuple, axis=1)          # tuple of [audience_average, audience_percent]
+    # movies['critic_reviews'] = movies[['critic_average', 'critic_percent']].apply(tuple, axis=1)                # tuple of [critic_average, critic_percent]
+    # movies = movies.drop(columns=['audience_ratings', 'audience_average', 'audience_percent', 'critic_average', 'critic_percent'])
+
+    # critic_reviews_vs_audience_reviews = movies[['critic_reviews', 'audience_reviews']]
+    # Remove rows with NaN values
+    # critic_reviews_vs_audience_reviews = critic_reviews_vs_audience_reviews[~critic_reviews_vs_audience_reviews.critic_reviews.apply(lambda x: np.isnan(x[0]) & np.isnan(x[1]))]
+    # critic_reviews_vs_audience_reviews = critic_reviews_vs_audience_reviews[~critic_reviews_vs_audience_reviews.audience_reviews.apply(lambda x: np.isnan(x[0]) & np.isnan(x[1]))]
+
+    # critic_review_vs_profit = movies[['critic_reviews', 'audience_reviews']]
+    # audience_reviews_vs_profit = movies[['audience_reviews', 'made_profit']]
+    
+
+    # Compute the correlation coefficients
+    critic_percent_vs_audience_reviews = movies[['critic_percent', 'audience_percent']].dropna()
+    critic_percent_vs_made_profit = movies[['critic_percent', 'made_profit']].dropna()
+    audience_percent_vs_made_profit = movies[['audience_percent', 'made_profit']].dropna()
+
+    critic_reviews_vs_audience_reviews = stats.linregress(critic_percent_vs_audience_reviews['critic_percent'], critic_percent_vs_audience_reviews['audience_percent']).rvalue
+    critic_reviews_vs_profit = stats.linregress(critic_percent_vs_made_profit['critic_percent'], critic_percent_vs_made_profit['made_profit']).rvalue
+    audience_reviews_vs_profit = stats.linregress(audience_percent_vs_made_profit['audience_percent'], audience_percent_vs_made_profit['made_profit']).rvalue
+
+    print("Correlation coefficient between critic reviews and audience reviews: {}".format(critic_reviews_vs_audience_reviews))
+    print("Correlation coefficient between critic reviews and profit/loss: {}".format(critic_reviews_vs_profit))
+    print("Correlation coefficient between audience reviews and profit/loss: {}".format(audience_reviews_vs_profit))
 
 
 if __name__ == "__main__":
