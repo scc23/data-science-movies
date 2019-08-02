fee2a32c88b92b35793c770965aaa53f43174e42
scc23 <sherman_chow@sfu.ca>
2019-08-01 17:23:48 -0700

    Train model to predict genres, and add visualizations

--- plot_summary_predict.py
+++ plot_summary_predict.py
@@ -13,13 +13,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import Pipeline
 import string
-# import nltk
-# nltk.download('stopwords')
-# nltk.download('punkt')
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+
+
 
 
 def remove_punctuations(summary):
@@ -32,10 +38,23 @@ def tokenize(x):
     token_words = [w for w in tokens if w.isalpha()]
     return token_words
 
+def generate_word_cloud(plot_summaries_words):
+    print('Generating word cloud image...')
+    summaries_text = plot_summaries_words.values
+    wordcloud = WordCloud(
+        width=800,
+        height=400,
+        background_color='white').generate(str(summaries_text))
+    # Display the generated word cloud.
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+    plt.show()
 
 
 def main():
     # Read JSON files into Pandas DataFrames
+    print('Reading data into DataFrames...')
     omdb_filename = "./movies/data/omdb-data.json.gz"
     rotten_filename = "./movies/data/rotten-tomatoes.json.gz"
     wikidata_filename = "./movies/data/wikidata-movies.json.gz"
@@ -64,6 +83,7 @@ def main():
     ]
     
     # Clean data.
+    print('Cleaning data...')
     movies_data = pd.merge(wikidata, plot_summaries, on='imdb_id')
     # Remove movies with no plot summary.
     movies_data = movies_data[movies_data['omdb_plot'] != 'N/A']
@@ -78,20 +98,11 @@ def main():
     stop_words.append('platform')
     stop_words.append('film')
     movies_data['omdb_plot'] = movies_data['omdb_plot'].apply(lambda x: [word for word in x if word not in stop_words])
-    # movies_data['omdb_text'] = movies_data['omdb_plot'].apply(lambda x: ' '.join(x))
+    movies_data['clean_summary'] = movies_data['omdb_plot'].apply(lambda x: ' '.join(x))
     plot_summaries_words = movies_data['omdb_plot'].apply(lambda x: ' '.join(x))
 
     # Create and generate word cloud image.
-    # summaries_text = plot_summaries_words.values
-    # wordcloud = WordCloud(
-    #     width=800,
-    #     height=400,
-    #     background_color='white').generate(str(summaries_text))
-    # Display the generated word cloud.
-    # plt.imshow(wordcloud, interpolation='bilinear')
-    # plt.axis("off")
-    # plt.tight_layout(pad=0)
-    # plt.show()
+    # generate_word_cloud(plot_summaries_words); return
 
     # Get all the genres from the movies.
     genres_all = []
@@ -119,26 +130,93 @@ def main():
     # print(movies_data2)
 
     # Get the number of counts for each genre.
-    genres_count = []
-    for col in movies_data2.columns[2:]:
-        genres_count.append(movies_data2[col].sum())
+    # genres_count = []
+    # for col in movies_data2.columns[2:]:
+    #     genres_count.append(movies_data2[col].sum())
 
     # Get the English label names for each genre.
-    genre_labels = []
-    for label in genres:
-        genre_labels.append(genre_map.get(label))
+    # genre_labels = []
+    # for label in genres:
+    #     genre_labels.append(genre_map.get(label))
 
     # print(genre_labels)
 
     # print(len(genres))
     # print(len(genres_count))
     
+    # print(movies_data.columns)
+    # print(movies_data2.columns)
 
-    # multilabel_binarizer = MultiLabelBinarizer()
+    print('Converting plot summaries to features...')
+    multilabel_binarizer = MultiLabelBinarizer()
+    multilabel_binarizer.fit(movies_data2['genre'])
+
+    # Extract features from cleaned plot summaries by usng tf-idf.
+    # tfidf_vectorizer = TfidfVectorizer()
+    tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=500)  # Use the 500 most frequent words in the data as features.
+
+    # Split data into train and validation data sets.
+    X = movies_data2['clean_summary']
+    y = multilabel_binarizer.transform(movies_data2['genre'])
+    
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=9)
+    # X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+
+    # Create tf-idf features.
+    X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+    X_valid_tfidf = tfidf_vectorizer.transform(X_valid)
+
+    # Build the genre prediction model.
+    print('Building prediction model...')
+    lr = LogisticRegression()
+    model = OneVsRestClassifier(lr)
+
+    # Train the model.
+    print('Training model...')
+    model.fit(X_train_tfidf, y_train)
+
+    # Print the score of the model on a validation subset of the data.
+    # print('Score of the model: {}'.format(model.score(X_valid_tfidf, y_valid)))
+
+    # Predict on validation data set.
+    print('Making predictions...')
+    y_prediction = model.predict_proba(X_valid_tfidf)
+    t = 0.25 # Threshold value.
+    y_prediction = (y_prediction >= t).astype(int)
+    predictions = multilabel_binarizer.inverse_transform(y_prediction)
+    # print(predictions)
+    res = pd.Series(predictions)
+    
+    print(res)
+    
+    print('\nf1 score: {}\n'.format(f1_score(y_valid, y_prediction, average="micro")))
 
 
-    # movies_data2.to_csv('genre_predictions.csv', index=False)
-    # movies_data.to_csv('genre_predictions.csv', index=False)
+    # Show 10 genre predictions, and compare it with the actual genres.
+    def make_predictions(q):
+        q_vec = tfidf_vectorizer.transform([q])
+        q_pred = model.predict_proba(q_vec)
+        t = 0.25
+        q_pred = (q_pred >= t).astype(int)
+        return multilabel_binarizer.inverse_transform(q_pred)
+
+    for i in range(10):
+        k = X_valid.sample(1).index[0]
+        predicted_genre = make_predictions(X_valid[k])
+        actual_genre = movies_data2['genre'][k]
+
+        predicted_genre_labels = []
+        for code_set in predicted_genre:
+            for code in code_set:
+                predicted_genre_labels.append(genre_map.get(code))
+        
+        actual_genre_labels = []
+        for code in actual_genre:
+            actual_genre_labels.append(genre_map.get(code))
+
+        print('IMDB ID: {}'.format(k))
+        print('\tPredicted genre: {}\n\tActual genre: {}\n'.format(predicted_genre_labels, actual_genre_labels))
+
     
 
 if __name__ == "__main__":
